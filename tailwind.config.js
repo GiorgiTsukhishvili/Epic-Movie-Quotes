5a1e744bc3193b339ad2e70f8693d4218269c130
giorgi <gcuxishvili@gmail.com>
2022-12-26 19:33:55 +0400

    feat: add 404 page with language and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-gradient':
+          'linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%)',
+      },
+      colors: {
+        zinc: {
+          150: '#F0F0F0',
+        },
+        red: {
+          650: '#E31221',
+        },
+      },
+    },
   },
   plugins: [],
 };
