f2d1f5c3db720fb0a41394c87301e2c7ad334837
giorgi <gcuxishvili@gmail.com>
2023-01-04 13:12:10 +0400

    fix: add language to register request

--- services/userRegister.ts
+++ services/userRegister.ts
@@ -1,6 +1,7 @@
 import { RegistrationTypes } from 'components/landing/Registration/registrationTypes';
 import { instance } from 'services';
+import { i18n } from 'next-i18next';
 
 export const sendUserRegisterData = (data: RegistrationTypes) => {
-  return instance.post('/register', data);
+  return instance.post('/register', { ...data, lang: i18n?.language });
 };
