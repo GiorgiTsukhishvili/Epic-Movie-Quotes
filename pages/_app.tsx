540c3b3627647b50021024defdd613452c27c602
giorgi <gcuxishvili@gmail.com>
2023-01-05 17:26:46 +0400

    fix/fix-email-verificatio-error-after-first-review

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation, i18n } from 'next-i18next';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <div
-      className={`${
-        i18n?.language === 'ka' ? 'font-helvetica-ka' : 'font-helvetica-en'
-      }`}
-    >
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState} />
+      <div
+        className={`${
+          i18n?.language === 'ka' ? 'font-helvetica-ka' : 'font-helvetica-en'
+        }`}
+      >
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
