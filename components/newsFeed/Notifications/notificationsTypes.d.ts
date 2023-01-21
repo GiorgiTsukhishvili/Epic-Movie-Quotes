80671b8512f8841afad1941330ad1924f4740462
giorgi <gcuxishvili@gmail.com>
2023-01-21 12:47:36 +0400

    feat: implement websocket

---
+++ components/newsFeed/Notifications/notificationsTypes.d.ts
@@ -0,0 +1,9 @@
+export type NotificationsTypes = {
+  user_id: number;
+  person_id: number;
+  is_new: boolean;
+  is_comment: boolean;
+  quote_id: number;
+  created_at: string;
+  id: number;
+};
--- components/newsFeed/Notifications/useNotifications.ts
+++ components/newsFeed/Notifications/useNotifications.ts
@@ -1,10 +1,19 @@
 import { useTranslation } from 'next-i18next';
 import { useEffect, useRef, useState } from 'react';
+import { getUserNotifications } from 'services';
+import { useSelector } from 'react-redux';
+import { UserTypes } from 'types';
+import { pusher } from 'config';
+import { NotificationsTypes } from './notificationsTypes';
+import { useQuery } from 'react-query';
 
 const useNotifications = () => {
   const [isNotificationsOpen, setIsNotificationsOpen] =
     useState<boolean>(false);
   const ref = useRef<HTMLDivElement>(null);
+  const {
+    user: { id },
+  } = useSelector((state: { user: UserTypes }) => state);
 
   const { t } = useTranslation();
 
@@ -14,13 +23,38 @@ const useNotifications = () => {
     }
   };
 
+  const [notifications, setNotifications] = useState<NotificationsTypes[]>([]);
+
+  useQuery('notifications', getUserNotifications, {
+    onSuccess: (data) => {
+      setNotifications(data.data);
+    },
+  });
+
+  useEffect(() => {
+    pusher();
+
+    if (id) {
+      (window as any).Echo.private(`epic-movies.${id}`).listen(
+        '.notifications',
+        (e: { notifications: { data: NotificationsTypes } }) => {
+          if (e.notifications.data.user_id === id) {
+            const newNotifications = notifications;
+            newNotifications.unshift(e.notifications.data);
+            setNotifications(newNotifications);
+          }
+        }
+      );
+    }
+  }, [id]);
+
   useEffect(() => {
     window.addEventListener('click', (e) => closeDropdown(e));
 
     return () => window.removeEventListener('click', (e) => closeDropdown(e));
   }, [isNotificationsOpen]);
 
-  return { isNotificationsOpen, setIsNotificationsOpen, t, ref };
+  return { isNotificationsOpen, setIsNotificationsOpen, t, ref, notifications };
 };
 
 export default useNotifications;
